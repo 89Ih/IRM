72dfece5ee5951311f8de048fb25ea3e697f2a1f
89Ih <ehabhamza94@gmail.com>
2024-04-03 21:53:24 +0200

    spinner

---
+++ client/src/components/Loading/spinner.jsx
@@ -0,0 +1,32 @@
+const Spinner = () => {
+    return (
+        <div class="d-flex align-items-center">
+            <div class="spinner-grow text-primary spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-secondary spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-success spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-danger spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-warning spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-info spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-light spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+            <div class="spinner-grow text-dark spinner-grow-sm" role="status">
+                <span class="sr-only"></span>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -2,15 +2,19 @@ import { useContext, useState } from "react";
 import "./Login.css";
 import { AuthContext } from "../../context/auth.context";
 import authService from "../../services/auth.service";
+import Spinner from "../../components/Loading/spinner";
+
 
 const LoginPage = () => {
   const { content ,storeToken,authenticateUser, styles,mode } = useContext(AuthContext);
   const {border}=styles 
+  const [spinned, setSpinned] = useState(false);
   const [email, seEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMSG, setErrorMSG] = useState(undefined);
 
   const handleSubmit=(e)=> {
+    setSpinned(true)  
     e.preventDefault();
     var data ={
       email,
@@ -19,43 +23,51 @@ const LoginPage = () => {
     authService
       .login(data)
       .then((res) => {
-        storeToken(res.data.authToken);
-        authenticateUser();
+
+         storeToken(res.data.authToken);
+         authenticateUser();
+         setSpinned(false)
       })
       .catch((err) => {
         console.error(err);
         setErrorMSG(err.response.data.message)
+        setSpinned(false)
       });
   }
   return (
-    <div className="_ff w-100 p-5" >
+    <div className="_ff w-100 p-5">
       <form className="_ff _form_login" onSubmit={handleSubmit}>
         <h1>{content.login}</h1>
         <input
           type="text"
           placeholder={`${content.email} :`}
           className="_ipt"
-          style={{border}}
+          style={{ border }}
           value={email}
-          autocomplete="username"
+          autoComplete="username"
           onChange={(event) => seEmail(event.target.value)}
         />
         <input
           type="password"
           placeholder={`${content.password} :`}
           className="_ipt"
-          style={{border}}
+          style={{ border }}
           value={password}
-          autocomplete="current-password"
+          autoComplete="current-password"
           onChange={(event) => setPassword(event.target.value)}
         />
-        <button type="submit" className={`_btn ${mode === 'light' ? '_btn_light' :'_btn_dark'}`}  style={{border}}>
+        <button
+          type="submit"
+          className={`_btn  ${mode === "light" ? "_btn_light" : "_btn_dark"}`}
+          style={{ border }}
+        >
           {content.signin}
         </button>
+         {spinned === true && <Spinner/>}
         <p>{errorMSG}</p>
       </form>
     </div>
   );
 };
-// admin@mail.com
+
 export default LoginPage;
